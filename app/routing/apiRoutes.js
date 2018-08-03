e0c7bc6c1f3c0bb18092c853edd690932f5d9aab
Cerise <developerdanielle@gmail.com>
2018-08-03 13:18:24 -0700

    packages

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,18 @@
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var path = require("path");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-// // Sets up the Express App
-// var app = express();
-// var PORT = process.env.PORT || 3000;
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-// // Sets up the Express app to handle data parsing
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
-// // Starts the server to begin listening
-// app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-//   });
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
   
